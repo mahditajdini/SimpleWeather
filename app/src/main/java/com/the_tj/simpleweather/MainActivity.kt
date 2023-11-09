12de7a0b19f17cdf216d24194d71c3404e9076ad
Mahdi tajdini <mahditajdini99@gmail.com>
2023-11-09 18:40:07 +0330

    last change

--- app/src/main/java/com/the_tj/simpleweather/MainActivity.kt
+++ app/src/main/java/com/the_tj/simpleweather/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //this is my change
         //new change
+        //my last change
     }
 }
