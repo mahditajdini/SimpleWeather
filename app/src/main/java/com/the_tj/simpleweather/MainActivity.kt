2f3445f13321aaa77e706c747b65a635ae06b5ea
Mahdi tajdini <mahditajdini99@gmail.com>
2023-11-19 23:18:49 +0330

    check if location is enabled

--- app/src/main/java/com/the_tj/simpleweather/MainActivity.kt
+++ app/src/main/java/com/the_tj/simpleweather/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.the_tj.simpleweather
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(!isLocationEnabled()){
+            Toast.makeText(this,"enable gps",Toast.LENGTH_SHORT).show()
+            val intent=Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"gps is enabled",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+    private fun isLocationEnabled():Boolean{
+        //provide access to the location services
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
 
     }
 }
